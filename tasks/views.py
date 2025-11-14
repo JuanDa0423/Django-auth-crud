fe62c7cd326a09e50cd0abc7f044824ed1fa6a44
JuanDa0423 <juanoquendo9@gmail.com>
2025-11-14 13:00:33 -0500

    Fix settings.py for Render deployment

--- tasks/views.py
+++ tasks/views.py
@@ -112,9 +112,33 @@ def delete_task(request, task_id):
     
 #funcion para cerrat sesion
 @login_required
-def signout(request):
-    logout(request)
-    return redirect('home')
+def Signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm()
+        })
+
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'],
+                    password=request.POST['password1']
+                )
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm(),
+                    "error": 'Username already exists'
+                })
+
+        return render(request, 'signup.html', {
+            'form': UserCreationForm(),
+            'error': 'Password do not match'
+        })
 
 #funcion para iniciar sesion
 def signin(request):
